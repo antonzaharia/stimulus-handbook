da18e7802b60e9791405c394a10d1a40c01fec22
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-15 14:26:41 +0000

    form validation

--- app/javascript/controllers/tweet_form_controller.js
+++ app/javascript/controllers/tweet_form_controller.js
@@ -1,8 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="tweet-form"
 export default class extends Controller {
-  static targets = ["body", "characterCount"]
+  static targets = ['body', 'characterCount']
   connect() {
     this.update()
   }
@@ -12,10 +12,23 @@ export default class extends Controller {
     let message = ''
 
     if (count === 1) {
-      message = "1 character"
+      message = '1 character'
     } else {
       message = `${count} characters`
     }
     this.characterCountTarget.textContent = message
+
+    if (count > 140) {
+      this.characterCountTarget.classList.add('text-orange-500')
+    } else {
+      this.characterCountTarget.classList.remove('text-orange-500')
+    }
+  }
+
+  submit(event) {
+    let count = this.bodyTarget.value.length
+    if (count > 140) {
+      event.detail.formSubmission.stop()
+    }
   }
 }
