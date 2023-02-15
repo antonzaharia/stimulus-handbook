ca91c77084a5daad685ec7fbfe954b63624c31e6
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-15 13:38:37 +0000

    characted count updates

--- app/javascript/controllers/tweet_form_controller.js
+++ app/javascript/controllers/tweet_form_controller.js
@@ -4,8 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["body", "characterCount"]
   connect() {
-    console.log(this.bodyTarget)
-    console.log(this.characterCountTarget)
+    this.update()
   }
 
   update() {
