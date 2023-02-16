ad056a4b6c14044105940103ec80274f130be815
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-16 17:10:39 +0000

    fixed like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,29 +1,19 @@
 class LikesController < ApplicationController
-  def new
-    @like = Likes.find(params[:id])
-  end
-
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @like = @tweet.likes.find { |like| like.user_id == current_user.id }
 
-    if @like
-      # Unlike
-      @like.destroy!
-      respond_to do |format|
-        format.html { redirect_to tweets_path }
-        format.turbo_stream
-      end
-    else
-      # Like
-      if @like = @tweet.likes.create!(user: current_user)
-        respond_to do |format|
-          format.html { redirect_to tweets_path }
-          format.turbo_stream
-        end
+    respond_to do |format|
+      if @like
+        # Unlike
+        @like.destroy!
+        @like = @tweet.likes.new
       else
-        render :new, status: :unprocessable_entity
+        # Like
+        @like = @tweet.likes.create!(user: current_user)
       end
+      format.html { redirect_to tweets_path }
+      format.turbo_stream
     end
   end
 end
