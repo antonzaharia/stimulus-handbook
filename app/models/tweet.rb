4b8be1b70a9b0d1b9450eee9e5ea105dcdb3d87d
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-16 10:06:12 +0000

    Likes added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :ideas, inverse_of: :tweet
+  has_many :likes, dependent: :destroy
+
   accepts_nested_attributes_for :ideas, reject_if: :all_blank, allow_destroy: true
+
+  def liked?(user)
+    !!self.likes.any?{ |like| like.user_id == user.id }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_145910) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_095249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_145910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tweet_id"], name: "index_ideas_on_tweet_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -44,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_145910) do
   end
 
   add_foreign_key "ideas", "tweets"
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "tweets", "users"
 end
