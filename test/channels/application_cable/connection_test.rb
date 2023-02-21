8fb4830d5568f487a6946482891cfe7af6afd904
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-21 15:10:30 +0000

    add current_user tp action cable connection dd current_user tp action

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -8,4 +8,19 @@ class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   #
   #   assert_equal connection.user_id, "42"
   # end
+
+  test "connects with devise" do
+    user = users(:one)
+    connect env: { 'warden' => FakeEnd.new(user) }
+    assert_equal connection.current_user, user
+  end
+
+  private
+
+  class FakeEnd
+    attr_reader :user
+    def initialize(user)
+      @user = user
+    end
+  end
 end
