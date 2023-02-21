adc0f3eccc5e8f742037f6a7452a1959dccd87ef
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-21 12:35:04 +0000

    test the UpdateUserTest job

--- test/jobs/update_person_job_test.rb
+++ test/jobs/update_person_job_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UpdateUserJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "update score" do
+    UpdateUserJob.perform_now(@user.id, 9)
+    assert_equal 9, @user.reload.score
+  end
 end
