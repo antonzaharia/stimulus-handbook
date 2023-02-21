533f7e6d86eeaa0220e4eae0cc1a695d2816b0f0
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-21 14:45:53 +0000

    on_trial? tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  # Freeze the time
   test "updated timestamp should be set to the current time" do
     freeze_time
-    user = users(:one)
-    user.update(name: 'test')
-    assert_equal Time.current, user.updated_at
+    @user.update(name: 'test')
+    assert_equal Time.current, @user.updated_at
+  end
+
+  test "has 7 day free trial" do
+    assert @user.on_trial?
+  end
+
+  # Travel in the future
+  test "7 days trial finished" do
+    travel 8.days do
+      assert_not @user.on_trial?
+    end
   end
 end
