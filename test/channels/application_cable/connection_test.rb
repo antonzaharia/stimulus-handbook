6f5701c24d6b3131b7b952df6ed84fb74517dcbe
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-21 15:16:04 +0000

    refactor connection to devise in tests

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -4,17 +4,21 @@ class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
 
   test "connects with devise" do
     user = users(:one)
-    connect env: { 'warden' => FakeEnd.new(user) }
+    connect_with(user)
     assert_equal connection.current_user, user
   end
 
   test "unauthorized without devise" do
     assert_raises ActionCable::Connection::Authorization::UnauthorizedError do
-      connect env: { 'warden' => FakeEnd.new(nil) }
+      connect_with(nil)
     end
   end
 
   private
+
+  def connect_with(user)
+    connect env: { 'warden' => FakeEnd.new(user) }
+  end
 
   class FakeEnd
     attr_reader :user
